b76c5348e434ccea7abedace981357a3ef544b34
baixiaoyong <baixiaoyong@joinquant.com>
2019-01-24 17:53:57 +0800

    fix

--- i18n/build_trans.sh
+++ i18n/build_trans.sh
@@ -1,3 +1,4 @@
 pybabel compile -D notebook -f -l zh_CN -i zh_CN/LC_MESSAGES/notebook.po -o zh_CN/LC_MESSAGES/notebook.mo
 pybabel compile -D nbui -f -l zh_CN -i zh_CN/LC_MESSAGES/nbui.po -o zh_CN/LC_MESSAGES/nbui.mo
 po2json -p -F -f jed1.x -d nbjs zh_CN/LC_MESSAGES/nbjs.po zh_CN/LC_MESSAGES/nbjs.json
+
